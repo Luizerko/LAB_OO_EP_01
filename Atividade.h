c7ce361e124b5fe7000a9c44915a5713e4a9de8b
Luizerko <31456074+Luizerko@users.noreply.github.com>
2018-08-24 22:46:14 -0300

    header Atividade
    Definição da classe Atividade

---
+++ Atividade.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "Pessoa.h"
+
+using namespace std;
+
+#define MAXIMO_RECURSOS 10
+
+#ifndef ATIVIDADE_H
+#define ATIVIDADE_H
+
+
+class Atividade {
+public:
+    Atividade(string nome, int horasNecessarias);
+    virtual ~Atividade();
+
+    string getNome();
+    int getHorasNecessarias();
+
+    bool adicionar(Pessoa* recurso);
+    Pessoa** getPessoas();
+    int getQuantidadeDePessoas();
+
+    int getDuracao();
+
+    double getCusto();
+
+    void imprimir();
+
+private:
+    string nome;
+    int horasNecessarias;
+    int contadorPessoas = 0;
+    Pessoa** vetorPessoas = new Pessoa*;
+};
+
+#endif // ATIVIDADE_H
